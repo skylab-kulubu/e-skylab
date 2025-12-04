ca4f15723e782d8825098ff8cd37cfc6f607c172
yustyy <yusuf.acmaci@hotmail.com>
2025-12-04 04:10:39 +0300

    fix: change HTTP method for assigning roles endpoint from POST to PUT

--- gateway/src/main/java/com/skylab/gateway/core/config/SecurityConfig.java
+++ gateway/src/main/java/com/skylab/gateway/core/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                         .pathMatchers(HttpMethod.POST, "/api/users/me/profile-picture").authenticated()
 
                         .pathMatchers(HttpMethod.POST, "/api/groups/**").hasAnyRole(ADMIN_ROLES)
-                        .pathMatchers(HttpMethod.POST, "/api/users/assign-role/**").authenticated()
+                        .pathMatchers(HttpMethod.PUT, "/api/users/assign-role/**").authenticated()
 
                         .pathMatchers(HttpMethod.GET, "/api/users/**").hasAnyRole(ADMIN_ROLES)
                         .pathMatchers(HttpMethod.PUT, "/api/users/{id}").hasAnyRole(ADMIN_ROLES)
