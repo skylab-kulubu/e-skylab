8aa30b660c084421c28d28203d51c750cc1c1ec8
yustyy <yusuf.acmaci@hotmail.com>
2026-05-09 01:32:00 +0300

    corsconfig updated on gateway

--- gateway/src/main/java/com/skylab/gateway/core/config/CorsConfig.java
+++ gateway/src/main/java/com/skylab/gateway/core/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001", "http://localhost:8081", "http://localhost:8080" ,"https://api.yildizskylab.com", "https://app.yildizskylab.com", "https://forms.yildizskylab.com", "https://agc.yildizskylab.com", "https://admin.yildizskylab.com", "https://e.yildizskylab.com"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:3001", "http://localhost:8081", "http://localhost:8080", "https://skylapp.yildizskylab.com", "https://api.yildizskylab.com", "https://app.yildizskylab.com", "https://forms.yildizskylab.com", "https://agc.yildizskylab.com", "https://admin.yildizskylab.com", "https://e.yildizskylab.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
