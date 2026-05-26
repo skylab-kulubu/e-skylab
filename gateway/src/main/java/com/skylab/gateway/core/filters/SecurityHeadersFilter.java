502c0d2c1ecab994832408601d95ca3a37059343
yustyy <yusuf.acmaci@hotmail.com>
2026-05-26 18:31:04 +0300

    bugfix on gateway

--- gateway/src/main/java/com/skylab/gateway/core/filters/SecurityHeadersFilter.java
+++ gateway/src/main/java/com/skylab/gateway/core/filters/SecurityHeadersFilter.java
@@ -3,7 +3,6 @@ package com.skylab.gateway.core.filters;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
@@ -13,8 +12,8 @@ public class SecurityHeadersFilter implements GlobalFilter, Ordered {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            HttpHeaders headers = exchange.getResponse().getHeaders();
+        exchange.getResponse().beforeCommit(() -> {
+            var headers = exchange.getResponse().getHeaders();
             headers.set("X-Content-Type-Options", "nosniff");
             headers.set("X-Frame-Options", "DENY");
             headers.set("X-XSS-Protection", "1; mode=block");
@@ -23,11 +22,13 @@ public class SecurityHeadersFilter implements GlobalFilter, Ordered {
             headers.set("Permissions-Policy", "geolocation=(), microphone=(), camera=(), payment=()");
             headers.set("Content-Security-Policy",
                     "default-src 'self'; " +
-                    "script-src 'self' 'unsafe-inline'; " +
-                    "style-src 'self' 'unsafe-inline'; " +
-                    "img-src 'self' data: https:; " +
-                    "connect-src 'self'");
-        }));
+                            "script-src 'self' 'unsafe-inline'; " +
+                            "style-src 'self' 'unsafe-inline'; " +
+                            "img-src 'self' data: https:; " +
+                            "connect-src 'self'");
+            return Mono.empty();
+        });
+        return chain.filter(exchange);
     }
 
     @Override
